43272fef2af896b087f79150e23fb4408f36aa0a
Mikhail <48692715+Mikhail-Gl96@users.noreply.github.com>
2021-02-16 04:51:16 +0300

    try except in main

--- download_pics.py
+++ download_pics.py
@@ -5,11 +5,8 @@ from PIL import Image
 def load_and_save_img_from_url(url, path):
     response = requests.get(url=url, verify=False, stream=True)
     response.raise_for_status()
-    try:
-        image = Image.open(response.raw)
-        image.thumbnail((1080, 1080))
-        rgb_im = image.convert('RGB')
-        rgb_im.save(f'{path}')
-        return path
-    except IOError:
-        print(f"Unable to open image from {url}")
+    image = Image.open(response.raw)
+    image.thumbnail((1080, 1080))
+    rgb_im = image.convert('RGB')
+    rgb_im.save(f'{path}')
+    return path
--- main.py
+++ main.py
@@ -10,24 +10,18 @@ import vk_api
 
 def get_pics_max_number():
     temp_url = f'https://xkcd.com/info.0.json'
-    try:
-        response = requests.get(temp_url)
-        img_last_num = response.json()['num']
-        return img_last_num
-    except Exception as e:
-        print(f'Error: {e}')
+    response = requests.get(temp_url)
+    img_last_num = response.json()['num']
+    return img_last_num
 
 
 def get_pic_from_xkcd(numb, path):
     temp_url = f'https://xkcd.com/{numb}/info.0.json'
-    try:
-        response = requests.get(temp_url).json()
-        img_url = response['img']
-        img_comment = response['alt']
-        img_path = download_pics.load_and_save_img_from_url(img_url, path=os.path.join(path, img_url.split('/')[-1]))
-        return {'path': img_path, 'comment': img_comment}
-    except Exception as e:
-        print(f'Error: {e}')
+    response = requests.get(temp_url).json()
+    img_url = response['img']
+    img_comment = response['alt']
+    img_path = download_pics.load_and_save_img_from_url(img_url, path=os.path.join(path, img_url.split('/')[-1]))
+    return {'path': img_path, 'comment': img_comment}
 
 
 if __name__ == "__main__":
@@ -39,11 +33,13 @@ if __name__ == "__main__":
     dir_name = 'images'
     image_paths = os.path.join(base_path, dir_name)
     os.makedirs(image_paths, exist_ok=True)
-
-    random_img_num = random.randint(0, get_pics_max_number())
-    current_img = get_pic_from_xkcd(random_img_num, image_paths)
-    vk_api.create_post_on_group_wall(group_id=my_vk_group_id,
-                                     current_img=current_img,
-                                     my_vk_key=my_vk_key)
-    os.remove(current_img['path'])
+    try:
+        random_img_num = random.randint(0, get_pics_max_number())
+        current_img = get_pic_from_xkcd(random_img_num, image_paths)
+        vk_api.create_post_on_group_wall(group_id=my_vk_group_id,
+                                         current_img=current_img,
+                                         my_vk_key=my_vk_key)
+        os.remove(current_img['path'])
+    except Exception as e:
+        print(f'Error: {e}')
 
