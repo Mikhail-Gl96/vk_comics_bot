e4a77647506a4122e93b24274a85512e0f8d0c28
Mikhail <48692715+Mikhail-Gl96@users.noreply.github.com>
2021-02-19 19:55:14 +0300

    fix wrong spelling

--- download_pics.py
+++ download_pics.py
@@ -2,7 +2,7 @@ import requests
 from PIL import Image
 
 
-def load_and_save_one_size_snd_extension_img_from_url(url, path):
+def load_and_save_one_size_and_extension_img_from_url(url, path):
     response = requests.get(url=url, verify=False, stream=True)
     response.raise_for_status()
     image = Image.open(response.raw)
